c62c5b7e27029cfcab6252a2ac05f1bef0dea0aa
Richard <richardmusime6@gmail.com>
2024-09-08 18:33:38 +0200

    the continuation on node course

---
+++ study.js
@@ -0,0 +1,16 @@
+const http = require("http")
+
+const server = http.createServer((req,res)=>{
+  if (req.url === "/") {
+    res.writeHead(200,{"Content-Type":"text/html"})
+    res.end("<h1>Hello worlds!</h1>")
+  }
+  else if (req.url === "/error") {
+    res.writeHead(404,{"Content-Type":"text/html"})
+    res.end("<h1>Not Found!</h1>")
+  }
+})
+
+
+server.listen(3000,()=>{console.log("server started");
+})
---
+++ studyExpress.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.get("/data", (req, res) => {
+  fs.readFile("./data.json", (err, data) => {
+    res.setHeader("Content-Type","application/json")
+    if (err) {
+      console.log(err);
+    }
+    res.send(data);
+  });
+});
+
+app.get("/data/new", (req, res) => {
+  fs.readFile("./data.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    let prevData = JSON.parse(data);
+    prevData.push({
+      title: "Richard",
+      author: "Rich Kid In the House",
+      year: 1923,
+    });
+    fs.writeFile("./data.json", JSON.stringify(prevData), (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      res.end("success");
+    });
+  });
+});
+
+app.listen(3000, () => console.log("started the server"));
